23087e83187d3f1971dc7a85ce2adf34da31c9dc
sbollig1 <87632897+sbollig1@users.noreply.github.com>
2021-07-29 14:57:54 +0530

    Add files via upload

---
+++ Java/Activities/Activity12.java
@@ -0,0 +1,27 @@
+package activities;
+
+interface Addable {
+	
+	int add(int num1, int num2);
+}
+
+public class Activity12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Addable ad1;
+		Addable ad2;
+		
+		 ad1 = (a, b) -> (a + b);
+		 
+		 System.out.println(ad1.add(10, 20));
+		
+		 ad2 = (int a, int b) -> {
+			    return (a + b);
+			};
+			
+		System.out.println(ad2.add(13, 200));
+
+	}
+
+}
---
+++ Java/Activities/Activity13.java
@@ -0,0 +1,33 @@
+package activities;
+
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+
+
+public class Activity13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		Random indexGen = new Random();
+		
+		System.out.println("Enter the numbers, enter a ; at the end:");
+		while(scan.hasNextInt()) {
+		    list.add(scan.nextInt());
+		}
+		
+		Integer nums[] = list.toArray(new Integer[0]);
+		
+		int index = indexGen.nextInt(nums.length);
+        System.out.println("Index value generated: " + index);
+        System.out.println("Value in arary at generated index: " + nums[index]);
+ 
+        scan.close();
+
+	}
+
+}
